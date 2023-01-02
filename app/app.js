35e5053eff0fa3c1d12a75e37ad62c064345d375
Sebastian Faliani <nedase@gmail.com>
2023-01-02 20:17:56 -0300

    Acomodando carpetas

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () =>
+      console.log(`Servidor funcionando en puerto ${PORT}
+http://localhost:${PORT}`)
+);
+app.use(express.static("public"));
+
+/* HOME */
+app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+/* CARRITO */
+app.get("/carrito", (req, res) => {
+      res.sendFile(path.join(__dirname, "./views/home.html"));
+});
