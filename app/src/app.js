6cede2fda8a5de90bdb785fcb1a184c256472b5c
Sebastian Faliani <nedase@gmail.com>
2023-01-24 18:03:16 -0300

    Cambiando estructura a MVC, Trabajo en equipo

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3030;
-
+const homeRouter = require("./routes/home");
 app.listen(PORT, () =>
       console.log(`Servidor funcionando en puerto ${PORT} 
 http://localhost:${PORT}`)
@@ -10,29 +10,27 @@ http://localhost:${PORT}`)
 app.use(express.static("public"));
 
 /* HOME */
-app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "./views/home.html"));
-});
+app.use("/", homeRouter);
 
 /* CARRITO */
 app.get("/carrito", (req, res) => {
-      res.sendFile(path.join(__dirname, "./views/carrito.html"));
+      res.sendFile(path.join(__dirname, "../views/carrito.html"));
 });
 
 /* DESCRIPCION */
 app.get("/descripcion", (req, res) => {
-      res.sendFile(path.join(__dirname, "./views/descripcion.html"));
+      res.sendFile(path.join(__dirname, "../views/descripcion.html"));
 });
 /* LOGIN */
 app.get("/login", (req, res) => {
-      res.sendFile(path.join(__dirname, "./views/login.html"));
+      res.sendFile(path.join(__dirname, "../views/login.html"));
 });
 
 /* REGISTRO */
 app.get("/registro", (req, res) => {
-      res.sendFile(path.join(__dirname, "./views/registro.html"));
+      res.sendFile(path.join(__dirname, "../views/registro.html"));
 });
 /* BUSQUEDA */
 app.get("/buscador", (req, res) => {
-      res.sendFile(path.join(__dirname,"./views/buscador.html" ));
+      res.sendFile(path.join(__dirname, "../views/buscador.html"));
 });
