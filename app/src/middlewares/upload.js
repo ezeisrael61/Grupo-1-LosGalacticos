75eaa300761e86ecf95731bc59918e6aa67a8ea0
LeonelMamani04 <leo6633nelmail.com>
2023-02-22 11:48:15 -0300

    agregar y editar imagen

--- app/src/middlewares/upload.js
+++ app/src/middlewares/upload.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const storageImage = multer.diskStorage({
 	destination: function (req, file, callback) {
-		callback(null, './public/img/Climatización')
+		callback(null, './public/img/' + req.body.category)
 	},
 	filename: function (req, file, callback) {
 	callback(null,`${Date.now()}_products_${path.extname(file.originalname)}` )
