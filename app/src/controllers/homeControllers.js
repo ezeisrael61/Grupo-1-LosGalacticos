80a770895b3c9897d1a5353fa11d3b70baf6523b
Sebastian Faliani <nedase@gmail.com>
2023-04-16 18:33:36 -0300

    Se agregaron Relaciones en los modelos - Buscar,Listar,detalle,Listar por Categoria,ofertas,detacados

--- app/src/controllers/homeControllers.js
+++ app/src/controllers/homeControllers.js
@@ -2,14 +2,55 @@
 //const { readJSON } = require("../database");
 
 //const dbProducts = readJSON("products.json");
-const db =require("../database/models")
+
+const { Product, Category, Sequelize } = require("../database/models");
+const { Op } = Sequelize;
+
 module.exports = {
       index: (req, res) => {
-            db.Products.findAll()
-            .then((products)=> {
-                  return res.render("pruebaproduct", {products:products})
-            })
-            
+            const IMAGE_MAIN_PROMISE = Product.findAll({
+                  where: {
+                        sold: {
+                              [Op.gt]: 50,
+                        },
+                  },
+                  limit: 5,
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            });
+            const FEATURED_PROMISE = Product.findAll({
+                  where: {
+                        sold: {
+                              [Op.gt]: 50,
+                        },
+                  },
+                  limit: 5,
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            });
+            const INSALE_PROMISE = Product.findAll({
+                  where: {
+                        discount: {
+                              [Op.gt]: 0,
+                        },
+                  },
+                  order: [["discount", "DESC"]],
+                  limit: 5,
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            });
+            Promise.all([FEATURED_PROMISE, INSALE_PROMISE, IMAGE_MAIN_PROMISE])
+
+                  .then(([featured, inSale, imageMain]) => {
+                        //return res.render("pruebaproduct", { featured, inSale, imageMain, session: req.session });
+                        return res.render("home", {
+                              featured,
+                              inSale,
+                              imageMain,
+                              session: req.session,
+                        });
+                  })
+                  .catch((error) => console.log(error));
+
+            /***************************************************************** */
+
             /*const featured = dbProducts
                   .filter((product) => {
                         return product.sold > 50;
@@ -22,6 +63,5 @@ module.exports = {
                   .slice(0, 5);
             const imageMain = dbProducts;
             return res.render("home", { featured, inSale, imageMain, session: req.session });*/
-                 
       },
 };
--- app/src/controllers/productosControllers.js
+++ app/src/controllers/productosControllers.js
@@ -7,47 +7,95 @@
             return product.discount > 0;
       })
       .slice(0, 5);*/
-const db= require("../database/models");
+const { Product, Category, Sequelize } = require("../database/models");
+const { Op } = Sequelize;
+
+const INSALE_PROMISE = Product.findAll({
+      where: {
+            discount: {
+                  [Op.gt]: 0,
+            },
+      },
+      order: [["discount", "DESC"]],
+      limit: 5,
+      include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+});
 
 module.exports = {
-      /*carrito: (req, res) => {
+      carrito: (req, res) => {
             res.render("products/carrito", { inSale, session: req.session });
       },
       description: (req, res) => {
-            const product = dbProducts.find((product) => {
-                  return product.id === Number(req.params.id);
+            const PRODUCT_PROMISE = Product.findByPk(req.params.id, {
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
             });
-            res.render("products/descripcion", { product, inSale, session: req.session });
+
+            Promise.all([PRODUCT_PROMISE, INSALE_PROMISE])
+                  .then(([product, inSale, imageMain]) => {
+                        return res.render("products/descripcion", {
+                              product,
+                              inSale,
+                              session: req.session,
+                        });
+                  })
+                  .catch((error) => console.log(error));
       },
       filters: (req, res) => {
-            const category = req.params.category;
-            let products = "";
-            if (category === "all") {
-                  products = dbProducts;
+            let category = req.params.category;
+            if (category != "all") {
+                  Product.findAll({
+                        where: {
+                              "$subcategory.category.name$": category,
+                        },
+
+                        include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+                  }).then((products) => {
+                        return res.render("products/filters", { products, category, session: req.session });
+                  });
             } else {
-                  products = dbProducts.filter((product) => {
-                        return product.category == category;
+                  Product.findAll({
+                        include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+                  }).then((products) => {
+                        return res.render("products/filters", { products, category, session: req.session });
                   });
             }
-            res.render("products/filters", { products, category, session: req.session });
-            //res.send(product);
       },
       inSale: (req, res) => {
             const category = "🔥 ¡OFERTAS SÓLO POR HOY! 🔥;";
-            const products = dbProducts.filter((product) => {
+            Product.findAll({
+                  where: {
+                        discount: {
+                              [Op.gt]: 0,
+                        },
+                  },
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            }).then((products) => {
+                  res.render("products/filters", { products, category, session: req.session });
+            });
+            /* const products = dbProducts.filter((product) => {
                   return product.discount > 0;
             });
-            res.render("products/filters", { products, category, session: req.session });
+            res.render("products/filters", { products, category, session: req.session }); */
       },
       featured: (req, res) => {
             const category = "✨PRODUCTOS DESTACADOS ✨";
-            const products = dbProducts.filter((product) => {
+            Product.findAll({
+                  where: {
+                        sold: {
+                              [Op.gt]: 50,
+                        },
+                  },
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            }).then((products) => {
+                  res.render("products/filters", { products, category, session: req.session });
+            });
+            /*  const products = dbProducts.filter((product) => {
                   return product.sold > 50;
             });
-            res.render("products/filters", { products, category, session: req.session });
+            res.render("products/filters", { products, category, session: req.session }); */
       },
       buscar: (req, res) => {
-            let valor = req.query.valor;
+            /* let valor = req.query.valor;
             products = dbProducts.filter((product) => {
                   product.name = product.name.toLowerCase();
                   if (product.name.includes(valor.toLowerCase())) {
@@ -56,6 +104,17 @@ module.exports = {
             });
 
             category = "all";
-            res.render("products/filters", { products, category, session: req.session });
-      },*/
+            res.render("products/filters", { products, category, session: req.session }); */
+            let valor = req.query.valor;
+            category = "all";
+            Product.findAll({
+                  where: {
+                        name: { [Op.substring]: valor },
+                  },
+
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            }).then((products) => {
+                  return res.render("products/filters", { products, category, session: req.session });
+            });
+      },
 };
