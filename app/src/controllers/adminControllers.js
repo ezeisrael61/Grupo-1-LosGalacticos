0999452b7bcf18a1626ea4836a023e1fbea0d9c5
Sebastian Faliani <nedase@gmail.com>
2023-04-29 23:50:24 -0300

    Se elimina imagen del productos en el Update y Destroy

--- app/src/controllers/adminControllers.js
+++ app/src/controllers/adminControllers.js
@@ -1,6 +1,29 @@
 const { Product, Image, Category, Subcategory, Sequelize } = require("../database/models");
 const { Op } = Sequelize;
 const { validationResult } = require("express-validator");
+const fs = require("fs");
+let procedures = {
+      deleteImage: (id) => {
+            Product.findByPk(id, {
+                  include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
+            }).then((product) => {
+                  if (product.images[0].name != "default-image.png") {
+                        //return console.log(product.images[0].name);
+                        const MATCH = fs.existsSync(`./public/img/${product.subcategory.category.name}/`, product.images[0].name);
+                        if (MATCH) {
+                              try {
+                                    fs.unlinkSync(`./public/img/${product.subcategory.category.name}/${product.images[0].name}`);
+                              } catch (error) {
+                                    throw new Error(error);
+                              }
+                        } else {
+                              console.log("No se encontró el archivo");
+                        }
+                  }
+            });
+      },
+};
+
 module.exports = {
       index: (req, res) => {
             Product.findAll({
@@ -33,9 +56,6 @@ module.exports = {
                   .catch((error) => console.log(error));
       },
       store: (req, res) => {
-            /* Otra forma de sacar el id mas grande */
-            /* let lastId = Math.max(...dbProducts.map(product => product.id)); */
-
             const errors = validationResult(req);
             if (errors.isEmpty()) {
                   Product.create({
@@ -43,9 +63,7 @@ module.exports = {
                         price: req.body.price,
                         discount: req.body.discount,
                         description: req.body.description,
-                        //category:req.body.category,
                         idSubCategory: req.body.subcategory,
-                        //image:req.file ? req.file.filename : "default-image.png",
                         sold: req.body.sold,
                         stock: req.body.stock,
                   }).then((products) => {
@@ -56,23 +74,6 @@ module.exports = {
                               return res.redirect("/admin/products");
                         });
                   });
-                  //res.render("admin/products");
-
-                  /* let newProduct = {
-                        id: lastId + 1,
-                        name: req.body.name,
-                        price: req.body.price,
-                        discount: req.body.discount,
-                        description: req.body.description,
-                        category: req.body.category,
-                        subcategory: req.body.subcategory,
-                        image: req.file ? req.file.filename : "default-image.png",
-                        sold: req.body.sold,
-                        stock: req.body.stock,
-                  };
-                  dbProducts.push(newProduct);
-                  writeJSON("products.json", dbProducts);
-                  return res.redirect("/"); */
             } else {
                   const PRODUCT_ALL = Product.findByPk(req.params.id, {
                         include: [{ association: "subcategory", include: [{ association: "category" }] }, { association: "images" }],
@@ -113,43 +114,32 @@ module.exports = {
                         res.render("admin/product-edit", { productToEdit, category, subcategory, session: req.session });
                   })
                   .catch((error) => console.log(error));
-            /*let productId = Number(req.params.id);
-            let productToEdit = dbProducts.find((product) => {
-                  return product.id === productId;
-            });
-            res.render("admin/product-edit", { productToEdit, session: req.session });*/
       },
       update: (req, res) => {
             const errors = validationResult(req);
             const ID_PRODUCT = req.params.id;
-            
             if (errors.isEmpty()) {
                   Product.update(
-                  {
-                        name: req.body.name,
-                        price: req.body.price,
-                        discount: req.body.discount,
-                        description: req.body.description,
-/*                         category: req.body.category,
- */                     idSubCategory: req.body.subcategory,
-/*                         image: req.file ? req.file.filename : product.image,
- */                     sold: req.body.sold,
-                        stock: req.body.stock,
-                  },
-                  {
-                        where:{idProduct:ID_PRODUCT}
-                  })
-                  .then((image) => {Image.update(
-                  {
-                        name: req.file ? req.file.filename : image.name,
-                        idProduct: ID_PRODUCT,
-                  },
-                  {
-                        where:{idProduct:ID_PRODUCT}
-                  },
-                  )})
-                   .then(() => {
-                        return res.redirect("/admin/products");
+                        {
+                              name: req.body.name,
+                              price: req.body.price,
+                              discount: req.body.discount,
+                              description: req.body.description,
+                              idSubCategory: req.body.subcategory,
+                              sold: req.body.sold,
+                              stock: req.body.stock,
+                        },
+                        { where: { idProduct: ID_PRODUCT } }
+                  ).then((result) => {
+                        if (result) {
+                              if (!req.file) {
+                                    return res.redirect("/admin/products");
+                              } else {
+                                    procedures.deleteImage(ID_PRODUCT);
+                                    Image.update({ name: req.file.filename }, { where: { idProduct: ID_PRODUCT } });
+                                    return res.redirect("/admin/products");
+                              }
+                        }
                   });
             } else {
                   const PRODUCT_ALL = Product.findByPk(req.params.id, {
@@ -161,7 +151,6 @@ module.exports = {
                   const SUBCATEGORY_ALL = Subcategory.findAll({
                         include: [{ association: "products" }, { association: "category" }],
                   });
-
                   Promise.all([PRODUCT_ALL, CATEGORY_ALL, SUBCATEGORY_ALL])
                         .then(([productToEdit, category, subcategory]) => {
                               res.render("admin/product-edit", {
@@ -176,10 +165,10 @@ module.exports = {
                         .catch((error) => console.log(error));
             }
       },
-      // Delete - Delete one product from DB
       destroy: (req, res) => {
             // obtengo el id del req.params
             const PRODUCT_ID = req.params.id;
+            procedures.deleteImage(PRODUCT_ID);
 
             Image.destroy({
                   where: {
@@ -197,16 +186,5 @@ module.exports = {
                         return res.redirect("/admin/products");
                   })
                   .catch((error) => console.log(error));
-
-            // busco el producto a eliminar y lo borro del array
-            /*  dbProducts.forEach((product) => {
-                  if (product.id === productId) {
-                        let productToDestroy = dbProducts.indexOf(product);
-                        dbProducts.splice(productToDestroy, 1);
-                  }
-            });
-
-            writeJSON("products.json", dbProducts);
-            res.redirect("/"); */
       },
 };
